1369fa3f83a0df93ea7e9f4bb58b3cb7d718d23d
MrConorAE <26195325+MrConorAE@users.noreply.github.com>
2021-05-10 09:51:54 +1200

    Initialising repository with file, EasyGui & gitignore.

---
+++ ConorEager-MonsterCatalog.py
@@ -0,0 +1,3 @@
+# MONSTER CATALOG for Python
+# Copyright (c) Conor Eager, 2021. All rights reserved.
+# Developed as a submission for AS91897: Use advanced programming techniques to develop a computer program
