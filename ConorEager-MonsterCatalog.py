85db1c3ce1fc685d5593e639a3d90b118b77e014
MrConorAE <26195325+MrConorAE@users.noreply.github.com>
2021-05-13 18:08:25 +1200

    Added existing monster cards.

--- ConorEager-MonsterCatalog.py
+++ ConorEager-MonsterCatalog.py
@@ -7,9 +7,12 @@
 import easygui as eg
 
 # DATA STRUCTURES
+
+
 class Monster:
     """This class represents a monster card - it contains all the fields required for a monster (name, strength, speed, stealth and cunning).
     """
+
     def __init__(self, nme, str, spe, ste, cun):
         """Initialise a new Monster.
 
@@ -25,9 +28,22 @@ class Monster:
         self.speed = spe
         self.stealth = ste
         self.cunning = cun
-        
+
+
 # This is the list in which Monster objects are stored.
-monsters = []
+#   Monster(Name,           Str,Spd,Ste,Cun)
+monsters = [
+    Monster("Stoneling",    7,  1,  25, 15),
+    Monster("Vexscream",    1,  6,  21, 19),
+    Monster("Dawnmirage",   5,  15, 18, 22),
+    Monster("Blazegolem",   15, 20, 23, 6),
+    Monster("Websnake",     7,  15, 10, 5),
+    Monster("Moldvine",     21, 18, 14, 5),
+    Monster("Vortexwing",   19, 13, 19, 2),
+    Monster("Rotthing",     16, 7,  4,  12),
+    Monster("Froststep",    14, 14, 17, 4),
+    Monster("Wispghoul",    17, 19, 3,  2)
+]
 
 # MAIN MENU
 # This loop is the main menu of the program, which allows the user to select what they want to do.
@@ -53,4 +69,3 @@ while True:
     elif (choice == "quit"):
         # Quit the program.
         pass
-    
